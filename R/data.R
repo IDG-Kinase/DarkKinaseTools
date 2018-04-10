1a657cadab3417b5e4205d9c3cc6054ada9b0c01
Matthew Berginski <matthew.berginski@unc.edu>
2018-04-10 15:48:44 -0400

    updating data documentation for all_kinases list

--- R/data.R
+++ R/data.R
@@ -1,3 +1,30 @@
+#' Human Dark Kinases
+#'
+#' A dataset containing a list of dark kinases.
+#'
+#' @format A data frame with 162 rows and 2 columns
+#' \describe{
+#'   \item{hgnc_id}{The human dark kinase name}
+#' }
+#' @source NIH and the Dark Kinase DRGC
+"dark_kinase_list"
+
+#' All Human Kinases
+#'
+#' A dataset containing a list of human kinases. This list has several
+#' pseudokinases, as they are included in the dark kinase list.
+#'
+#' @format A data frame with 524 rows and 5 columns
+#' \describe{
+#'   \item{hgnc_id}{The human dark kinase ID from HGNC}
+#'   \item{symbol}{The human dark kinase symbol from HGNC}
+#'   \item{ensembl_gene_id}{ID from ensembl}
+#'   \item{class}{Either Dark or Light, depending on the understudied-ness}
+#'   \item{kinase_com_name}{The name used on the kinase.com list, used for cross-referencing other lists from kinase.com}
+#' }
+#' @source NIH, the Dark Kinase DRGC, HGNC and kinase.com
+"all_kinases"
+
 #' Mouse Kinases and Corresponding Human Orthologs
 #'
 #' A dataset containing a list of mouse kinases and human orthologs, if present.
@@ -11,14 +38,3 @@
 #' }
 #' @source \url{http://kinase.com/mouse/}
 "mouse_human_gene_names"
-
-#' Human Dark Kinases
-#'
-#' A dataset containing a list of dark kinases
-#'
-#' @format A data frame with 162 rows and 1 column
-#' \describe{
-#'   \item{hgnc_id}{The human dark kinase name}
-#' }
-#' @source NIH and the Dark Kinase DRGC
-"dark_kinase_list"
