deca4f2cc2a13d9c897044023d1b64f41b80258f
Matthew Berginski <matthew.berginski@unc.edu>
2018-04-11 15:01:26 -0400

    adding documentation file for overall package

---
+++ R/DarkKinaseTools.R
@@ -0,0 +1,12 @@
+#' DarkKinaseTools: A package for working with dark kinase data.
+#'
+#' The DarkKinaseTools package provides three data sets that I've found to be
+#' generally usefull for dealing with kinase data sets. These include a set of
+#' all human kinase, all dark human kinases and a set of mouse kinases and
+#' corresponding human kinases.
+#'
+#' @section Dataset
+#'
+#' @docType package
+#' @name DarkKinaseTools
+NULL
